77a9ce57a2d38bbf109be4cf1a6157f9eeea4dd7
SergGrebnev <serggrebnev79@gmail.com>
2024-05-25 23:23:03 +0700

    edit main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 123;   // стоимость билета
+        int bonus_mile_price = 20;  // количество рублей для одной бонусной мили
 
+        int bonus_mile= ticket_price/bonus_mile_price; // количество бонусных миль
+        System.out.println(bonus_mile);
+    }
 }
